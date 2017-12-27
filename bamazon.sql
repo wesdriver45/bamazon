ffda56007661409bbdbc48592fa41a3327c2cd31
wesdriver45 <wesdriver45@gmail.com>
2017-12-26 20:38:26 -0500

    sql page

--- bamazon.sql
+++ bamazon.sql
@@ -4,11 +4,17 @@ CREATE DATABASE bamazonDB;
 
 USE bamazonDB;
 
-CREATE TABLE products(
-item_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE products (
+id INT NOT NULL AUTO_INCREMENT,
+item_id INTEGER(11) NOT NULL,
 product_name VARCHAR(50) NOT NULL,
 department_name VARCHAR(50) NOT NULL,
 price DECIMAL(10,2) NOT NULL,
 stock_quantity INT NOT NULL,
 PRIMARY KEY (id)
 );
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (1, "Pots", "Housewares", 5.50, 100), (2, "Pans", "Housewares", 6.75, 103), (3, "Lotion", "Health and Beauty", 12.49, 73);
+
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
+//npm packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+//database connection
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
@@ -13,3 +14,7 @@ connection.connect(function(err) {
 	if (err) throw err;
 	console.log("connected as id " + connection.threadId)
 });
+
+function purchaseProduct() {
+	
+}
