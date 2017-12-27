a72ff25dea8e810922ae95335fc768204c8f272d
wesdriver45 <wesdriver45@gmail.com>
2017-12-26 19:10:51 -0500

    host connection

---
+++ bamazon.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products(
+item_id INT NOT NULL AUTO_INCREMENT,
+product_name VARCHAR(50) NOT NULL,
+department_name VARCHAR(50) NOT NULL,
+price DECIMAL(10,2) NOT NULL,
+stock_quantity INT NOT NULL,
+PRIMARY KEY (id)
+);
---
+++ bamazonCustomer.js
@@ -0,0 +1,15 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "All4myFam",
+	database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+	if (err) throw err;
+	console.log("connected as id " + connection.threadId)
+});
